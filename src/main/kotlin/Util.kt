28bdea41a63f239cecc9d4a3d9581ec92cfcdd82
Jan Tiemann <j.tiemann@hzdr.de>
2022-04-13 12:43:43 +0200

    Properties: introduce property file and util method to read

---
+++ src/main/kotlin/Util.kt
@@ -0,0 +1,28 @@
+@file:Suppress("unused")
+
+import java.io.FileInputStream
+import java.io.IOException
+import java.util.*
+
+
+fun getProperty(name:String): String? {
+    try {
+        FileInputStream("microscenery.properties").use { input ->
+            val prop = Properties()
+
+            // load a properties file
+            prop.load(input)
+
+            // get the property value and print it out
+            return prop.getProperty(name)
+        }
+    } catch (ex: IOException) {
+        throw Exception("Could not find property $name", ex)
+    }
+}
+
+fun getPropertyInt(name: String): Int{
+    val raw = getProperty(name) ?: throw IllegalStateException("Could not get property $name")
+    return (raw.toIntOrNull())?: throw IllegalStateException("Property $name cant be casted to Int")
+}
+
