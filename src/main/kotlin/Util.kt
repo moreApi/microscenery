a10d39ad8f65116055f90973cf2dc7c9197bf406
Jan Tiemann <j.tiemann@hzdr.de>
2022-05-12 16:57:39 +0200

    Util: Serialize Vector3i by hand

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -3,10 +3,17 @@
 @file:JvmMultifileClass
 
 import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.Serializer
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
 import com.esotericsoftware.kryo.util.DefaultInstantiatorStrategy
 import graphics.scenery.Settings
+import graphics.scenery.serialization.Vector3fSerializer
+import graphics.scenery.utils.LazyLogger
 import microscenery.network.ClientSignal
 import microscenery.network.ServerSignal
+import org.joml.Vector3f
+import org.joml.Vector3i
 import org.objenesis.strategy.StdInstantiatorStrategy
 
 
@@ -29,6 +36,8 @@ val ky = object : ThreadLocal<Kryo>() {
         kryo.setCopyReferences(true)
         kryo.register(ServerSignal::class.java)
         kryo.register(ClientSignal::class.java)
+        kryo.register(Vector3f::class.java, Vector3fSerializer())
+        kryo.register(Vector3i::class.java, Vector3iSerializer())
         return kryo
     }
 }
@@ -43,6 +52,24 @@ fun lightSleepOn(mills: Int = 1000, target: () -> Any? ){
     for (t in 1..10){
         if (target() == null)
             Thread.sleep(mills/10L)
+    }
+}
+
+class Vector3iSerializer: Serializer<Vector3i>() {
+    val logger by LazyLogger()
+    override fun write(kryo: Kryo, output: Output, vector: Vector3i) {
+        kryo.writeClassAndObject(output, intArrayOf(vector.x, vector.y, vector.z))
+//        logger.info("Serialized ${vector.x}/${vector.y}/${vector.z}")
+    }
+
+    override fun read(kryo: Kryo, input: Input, oobClass: Class<out Vector3i>): Vector3i {
+        val arr = kryo.readClassAndObject(input) as IntArray
+        return Vector3i(arr[0], arr[1], arr[2])
+    }
+
+    init {
+        // TODO: Should this be true or false?
+        isImmutable = false
     }
 }
 
