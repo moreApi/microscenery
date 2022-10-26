e3da30ae92473327e3a9a5ca1c694a06983952ee
Jan T <moreApi@users.noreply.github.com>
2022-10-26 15:37:38 +0200

    DemoHWScene.kt: Extract behavior to static function

--- src/test/kotlin/microscenery/example/DemoHWScene.kt
+++ src/test/kotlin/microscenery/example/DemoHWScene.kt
@@ -13,14 +13,7 @@ import microscenery.nowMillis
 import org.joml.Vector3f
 import kotlin.concurrent.thread
 
-private enum class Mode {
-    RandomStatic,
-    Fixed,
-    RandomLive
-}
-
 class DemoHWScene : DefaultScene() {
-
     init {
         logger.info("Starting demo hw scene")
 
@@ -39,57 +32,7 @@ class DemoHWScene : DefaultScene() {
         }
         scene.addChild(hullbox)
 
-        val sortedSlices = ArrayList<Vector3f>()
-        when (Mode.RandomLive) {
-            Mode.RandomStatic -> {
-                for (i in 0..200) {
-                    val target = Random.random3DVectorFromRange(0f, hw.side.toFloat())
-                    sortedSlices.add(target)
-                }
-            }
-            Mode.Fixed -> {
-                for (z in listOf(0, 50, 100, 150, 199))
-                    for (y in listOf(0, 50, 100, 150))
-                        for (x in listOf(0, 50, 100, 150)) {
-                            val target = Vector3f(x.toFloat(), y.toFloat(), z.toFloat())
-                            sortedSlices.add(target)
-                        }
-            }
-            Mode.RandomLive -> {
-                stageSpaceManager.live(true)
-                stageSpaceManager.focusFrame?.let { focus ->
-                    var start = Vector3f()
-                    var target = Vector3f()
-                    var startTime = 0L
-                    val travelTime = 3000
-                    var count = 0
-                    focus.update += {
-                        focus.spatial {
-
-                            if (nowMillis() > startTime + travelTime) {
-                                startTime = nowMillis()
-                                position = target
-                                start = position
-                                target = Random.random3DVectorFromRange(0f, hw.side.toFloat())
-                            }
-
-                            val dir = target - start
-                            val relPos = (nowMillis() - startTime) / travelTime.toFloat()
-                            position = start + (dir * relPos)
-                            stageSpaceManager.stagePosition = position
-                            if (count++ % 10 == 0) stageSpaceManager.snapSlice()
-                        }
-                    }
-                }
-            }
-        }
-        sortedSlices.sortBy { it.z() }
-        for(target in sortedSlices)
-        {
-            stageSpaceManager.stagePosition =  target
-            stageSpaceManager.snapSlice()
-        }
-
+        demoBehavior(DemoBehaviorMode.RandomLive,hw.side.toFloat(), stageSpaceManager)
 
         thread {
             while (true) {
@@ -104,6 +47,68 @@ class DemoHWScene : DefaultScene() {
         fun main(args: Array<String>) {
             DemoHWScene().main()
         }
-    }
 
+        fun demoBehavior(
+            mode: DemoBehaviorMode,
+            extend: Float,
+            stageSpaceManager: StageSpaceManager
+        ) {
+            val sortedSlices = ArrayList<Vector3f>()
+            when (mode) {
+                DemoBehaviorMode.RandomStatic -> {
+                    for (i in 0..200) {
+                        val target = Random.random3DVectorFromRange(0f, extend)
+                        sortedSlices.add(target)
+                    }
+                }
+                DemoBehaviorMode.Fixed -> {
+                    for (z in listOf(0, 50, 100, 150, 199))
+                        for (y in listOf(0, 50, 100, 150))
+                            for (x in listOf(0, 50, 100, 150)) {
+                                val target = Vector3f(x.toFloat(), y.toFloat(), z.toFloat())
+                                sortedSlices.add(target)
+                            }
+                }
+                DemoBehaviorMode.RandomLive -> {
+                    stageSpaceManager.live(true)
+                    stageSpaceManager.focusFrame?.let { focus ->
+                        var start = Vector3f()
+                        var target = Vector3f()
+                        var startTime = 0L
+                        val travelTime = 3000
+                        var count = 0
+                        focus.update += {
+                            focus.spatial {
+
+                                if (nowMillis() > startTime + travelTime) {
+                                    startTime = nowMillis()
+                                    position = target
+                                    start = position
+                                    target = Random.random3DVectorFromRange(0f, extend)
+                                }
+
+                                val dir = target - start
+                                val relPos = (nowMillis() - startTime) / travelTime.toFloat()
+                                position = start + (dir * relPos)
+                                stageSpaceManager.stagePosition = position
+                                if (count++ % 10 == 0) stageSpaceManager.snapSlice()
+                            }
+                        }
+                    }
+                }
+            }
+            sortedSlices.sortBy { it.z() }
+            for (target in sortedSlices) {
+                stageSpaceManager.stagePosition = target
+                stageSpaceManager.snapSlice()
+            }
+        }
+    }
+}
+
+
+enum class DemoBehaviorMode {
+    RandomStatic,
+    Fixed,
+    RandomLive
 }
