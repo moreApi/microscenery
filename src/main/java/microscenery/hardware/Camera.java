a7c2baea86ad472ce0c539c264e86f2ed9c2e6de
Jan T <moreApi@users.noreply.github.com>
2022-11-03 14:00:19 +0100

    Java Code: Auto Reformat Code

--- src/main/java/microscenery/hardware/Camera.java
+++ src/main/java/microscenery/hardware/Camera.java
@@ -7,50 +7,50 @@ import mmcorej.TaggedImage;
 public class Camera extends Device {
 
 
-	public Camera(CMMCore core, String label) {
-		super(core, label);
-	}
+    public Camera(CMMCore core, String label) {
+        super(core, label);
+    }
 
-	public TaggedImage snapImage() {
-		try {
-			core.snapImage();
-			return core.getTaggedImage();
-		} catch (Exception e) {
-			ReportingUtils.logError(e);
-			return null;
-		}
-	}
+    public TaggedImage snapImage() {
+        try {
+            core.snapImage();
+            return core.getTaggedImage();
+        } catch (Exception e) {
+            ReportingUtils.logError(e);
+            return null;
+        }
+    }
 
-	/**
-	 * Sets the exposure time in milliseconds.
-	 * 
-	 * @param exposureTime Time to open camera's physical shutter in milliseconds.
-	 */
-	public void setExposure(double exposureTime) {
-		try {
-			core.setExposure(exposureTime);
-		} catch (Exception e) {
-			ReportingUtils.Companion.logError(e);
-		}
-	}
+    /**
+     * Sets the exposure time in milliseconds.
+     *
+     * @param exposureTime Time to open camera's physical shutter in milliseconds.
+     */
+    public void setExposure(double exposureTime) {
+        try {
+            core.setExposure(exposureTime);
+        } catch (Exception e) {
+            ReportingUtils.Companion.logError(e);
+        }
+    }
 
-	/**
-	 * Gets the exposure time in milliseconds.
-	 * 
-	 * @return Camera's current exposure time, in ms.
-	 */
-	public double getExposure() {
-		try {
-			return core.getExposure();
-		} catch (Exception e) {
-			ReportingUtils.Companion.logError(e);
-			return -1;
-		}
-	}
+    /**
+     * Gets the exposure time in milliseconds.
+     *
+     * @return Camera's current exposure time, in ms.
+     */
+    public double getExposure() {
+        try {
+            return core.getExposure();
+        } catch (Exception e) {
+            ReportingUtils.Companion.logError(e);
+            return -1;
+        }
+    }
 
-	@Override
-	public DeviceType getMMType() {
-		return DeviceType.CameraDevice;
-	}
+    @Override
+    public DeviceType getMMType() {
+        return DeviceType.CameraDevice;
+    }
 
 }
--- src/main/java/microscenery/hardware/GenericRotator.java
+++ src/main/java/microscenery/hardware/GenericRotator.java
@@ -8,35 +8,35 @@ import java.util.Collection;
 public class GenericRotator extends Stage {
 
 
-	public GenericRotator(CMMCore core, String label) {
-		super(core, label);
-	}
+    public GenericRotator(CMMCore core, String label) {
+        super(core, label);
+    }
 
-	@Override
-	public double getVelocity() {
-		return 1.0D;
-	}
+    @Override
+    public double getVelocity() {
+        return 1.0D;
+    }
 
-	@Override
-	public void setVelocity(double velocity) throws IllegalArgumentException {
-		if (velocity != 1.0D)
-			throw new IllegalArgumentException("Invalid rotator velocity " + velocity);
+    @Override
+    public void setVelocity(double velocity) throws IllegalArgumentException {
+        if (velocity != 1.0D)
+            throw new IllegalArgumentException("Invalid rotator velocity " + velocity);
 
-		// nop
-	}
+        // nop
+    }
 
-	@Override
-	public Collection<Double> getAllowedVelocities() {
-		return Arrays.asList(1.0D);
-	}
+    @Override
+    public Collection<Double> getAllowedVelocities() {
+        return Arrays.asList(1.0D);
+    }
 
-	@Override
-	public double getMinPosition() {
-		return -180.0D;
-	}
+    @Override
+    public double getMinPosition() {
+        return -180.0D;
+    }
 
-	@Override
-	public double getMaxPosition() {
-		return +180.0D;
-	}
+    @Override
+    public double getMaxPosition() {
+        return +180.0D;
+    }
 }
