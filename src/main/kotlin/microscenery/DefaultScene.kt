1b27abdbaedd18016aa56f39dd4a0515610d2b35
Jan T <moreApi@users.noreply.github.com>
2022-11-03 13:52:03 +0100

    Default Scene: offset lights a bit

--- src/main/kotlin/microscenery/DefaultScene.kt
+++ src/main/kotlin/microscenery/DefaultScene.kt
@@ -17,13 +17,13 @@ open class DefaultScene(val initHook: ((scene:Scene, hub:Hub)-> Unit)? = null, n
         )
 
         val light = PointLight(radius = 15.0f)
-        light.spatial().position = Vector3f(2.0f, 0.0f, 2.0f)*2f
+        light.spatial().position = Vector3f(2.0f, 1.0f, 2.0f)*2f
         light.intensity = 15.0f
         light.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
         scene.addChild(light)
 
         val light2 = PointLight(radius = 15.0f)
-        light2.spatial().position = Vector3f(-2.0f, 0.0f, -2.0f)*2f
+        light2.spatial().position = Vector3f(-2.0f, -1.0f, -2.0f)*2f
         light2.intensity = 15.0f
         light2.emissionColor = Vector3f(1.0f, 1.0f, 1.0f)
         scene.addChild(light2)
