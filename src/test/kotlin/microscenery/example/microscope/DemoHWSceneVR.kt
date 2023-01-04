7223e5e02babf58c137f31e2b752c5ba3333f521
Jan T <moreApi@users.noreply.github.com>
2023-01-04 16:12:56 +0100

    DemoHWSceneVR.kt: VR controls vor live and steering

--- src/test/kotlin/microscenery/example/microscope/DemoHWSceneVR.kt
+++ src/test/kotlin/microscenery/example/microscope/DemoHWSceneVR.kt
@@ -1,33 +1,25 @@
 package microscenery.example.microscope
 
-import graphics.scenery.Sphere
-import graphics.scenery.controls.behaviours.Grabable
-import graphics.scenery.controls.behaviours.Touchable
+import graphics.scenery.controls.behaviours.*
 import graphics.scenery.volumes.Volume
 import microscenery.DefaultVRScene
 import microscenery.stageSpace.StageSpaceManager
 import microscenery.VRUI.VRUIManager
 import microscenery.hardware.DemoMicroscopeHardware
-import org.joml.Vector3f
+import microscenery.stageSpace.FocusFrame
 import kotlin.concurrent.thread
 
 class DemoHWSceneVR : DefaultVRScene() {
+
+    lateinit var stageSpaceManager: StageSpaceManager
 
     override fun init() {
         super.init()
 
 
         val hw = DemoMicroscopeHardware()
-        val stageSpaceManager = StageSpaceManager(hw, scene, hub, addFocusFrame = true, scaleDownFactor = 200f)
+        stageSpaceManager = StageSpaceManager(hw, scene, hub, addFocusFrame = true, scaleDownFactor = 200f)
 
-        Sphere(0.5f).apply {
-            spatial {
-                scale = Vector3f(10f)
-            }
-            addAttribute(Grabable::class.java, Grabable())
-            addAttribute(Touchable::class.java, Touchable())
-            stageSpaceManager.stageRoot.addChild(this)
-        }
 
         thread {
             while (true) {
@@ -40,7 +32,21 @@ class DemoHWSceneVR : DefaultVRScene() {
     override fun inputSetup() {
         super.inputSetup()
 
-        VRUIManager.initBehavior(scene, hmd, inputHandler) {
+        VRUIManager.initBehavior(scene, hmd, inputHandler,
+        customActions = WheelMenu(hmd, listOf(
+            Switch("Steering", false) { value ->
+                stageSpaceManager.focusTarget?.let {
+                    if (value && it.mode != FocusFrame.Mode.STEERING) {
+                        it.mode = FocusFrame.Mode.STEERING
+                    } else {
+                        it.mode = FocusFrame.Mode.PASSIVE
+                    }
+                    logger.info("focusframe mode is now ${it.mode}")
+                }
+            },
+            Switch("Live", false) { stageSpaceManager.hardware.live = it },
+        ))
+        ) {
             scene.findByClassname(Volume::class.simpleName!!).first() as Volume
         }
     }
