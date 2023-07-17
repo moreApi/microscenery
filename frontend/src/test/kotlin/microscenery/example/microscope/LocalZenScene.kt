8b387c3140e7e8b93715f5514f7abb1af0886241
Jan T <moreApi@users.noreply.github.com>
2023-07-17 11:23:03 +0200

    Rename ZenConnector.kt to ZenMicroscope.kt

--- frontend/src/test/kotlin/microscenery/example/microscope/LocalZenScene.kt
+++ frontend/src/test/kotlin/microscenery/example/microscope/LocalZenScene.kt
@@ -5,7 +5,7 @@ import microscenery.UI.StageSpaceUI
 import microscenery.hardware.MicroscopeHardware
 import microscenery.stageSpace.StageSpaceManager
 import microscenery.zenSysConCon.CZIFileWrapper
-import microscenery.zenSysConCon.ZenConnector
+import microscenery.zenSysConCon.ZenMicroscope
 import org.joml.Vector3f
 import kotlin.concurrent.thread
 
@@ -20,21 +20,21 @@ class LocalZenScene : DefaultScene(withSwingUI = true) {
         val id = """C:\Users\JanCasus\Zeiss\sd3\20230712_488_square_ring.czi"""
 
         val cziWrap = CZIFileWrapper(id)
-        val zenConnector = ZenConnector()
+        val zenMicroscope = ZenMicroscope()
 
-        val hardware: MicroscopeHardware = zenConnector
+        val hardware: MicroscopeHardware = zenMicroscope
         stageSpaceManager = StageSpaceManager(hardware, scene, hub)
 
         thread {
             Thread.sleep(2000)
 
-            zenConnector.stack(cziWrap)
+            zenMicroscope.stack(cziWrap)
         }
         thread {
             while (true){
                 Thread.sleep(200)
                 print(stageSpaceManager)
-                print(zenConnector)
+                print(zenMicroscope)
             }
         }
     }
