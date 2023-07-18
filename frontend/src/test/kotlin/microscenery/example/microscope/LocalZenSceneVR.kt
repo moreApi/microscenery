921b8e48f364a7a14b37004650293e8ff8895cdd
Jan T <moreApi@users.noreply.github.com>
2023-07-18 17:42:58 +0200

    LocalZenScene: Add VR variant + hide focus frames

--- frontend/src/test/kotlin/microscenery/example/microscope/LocalZenSceneVR.kt
+++ frontend/src/test/kotlin/microscenery/example/microscope/LocalZenSceneVR.kt
@@ -1,7 +1,8 @@
 package microscenery.example.microscope
 
-import microscenery.DefaultScene
+import microscenery.DefaultVRScene
 import microscenery.UI.StageSpaceUI
+import microscenery.VRUI.VRUIManager
 import microscenery.hardware.MicroscopeHardware
 import microscenery.stageSpace.StageSpaceManager
 import microscenery.zenSysConCon.CZIFileWrapper
@@ -9,15 +10,17 @@ import microscenery.zenSysConCon.ZenMicroscope
 import org.joml.Vector3f
 import kotlin.concurrent.thread
 
-class LocalZenScene : DefaultScene(withSwingUI = true) {
-    val stageSpaceManager: StageSpaceManager
+class LocalZenSceneVR : DefaultVRScene() {
+    lateinit var  stageSpaceManager: StageSpaceManager
 
-    init {
+    override fun init() {
+        super.init()
         cam.spatial().position = Vector3f(0f, 0f, 5f)
 
 
         //val id = """C:\Users\JanCasus\Zeiss\Experiment-19.czi"""
-        val id = """C:\Users\JanCasus\Zeiss\sd3\20230712_488_square_ring.czi"""
+        //val id = """C:\Users\JanCasus\Zeiss\sd3\20230712_488_square_ring.czi"""
+        val id = """C:\Users\JanCasus\Zeiss\sd3\20230712_488_square_ring3.czi"""
 
         val cziWrap = CZIFileWrapper(id)
         val zenMicroscope = ZenMicroscope()
@@ -34,21 +37,31 @@ class LocalZenScene : DefaultScene(withSwingUI = true) {
         thread {
             while (true){
                 Thread.sleep(200)
-                print(stageSpaceManager)
-                print(zenMicroscope)
+                stageSpaceManager to zenMicroscope
             }
         }
     }
 
     override fun inputSetup() {
         super.inputSetup()
-        StageSpaceUI(stageSpaceManager).stageUI(this,inputHandler)
+        val ssUI = StageSpaceUI(stageSpaceManager)
+
+        inputHandler?.let {
+            ssUI.stageKeyUI(it, cam)
+        }
+
+        VRUIManager.initBehavior(
+            scene, hmd, inputHandler,
+            stageSpaceUI = ssUI
+        ) {
+            stageSpaceManager.stageRoot
+        }
     }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            LocalZenScene().main()
+            LocalZenSceneVR().main()
         }
     }
 }
