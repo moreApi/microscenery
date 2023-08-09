2ebf821068bd05972da9b67c0ceaccfa95f47ed5
Jan T <moreApi@users.noreply.github.com>
2023-08-09 14:59:15 +0200

    WIP box ablation

--- frontend/src/main/kotlin/microscenery/Util.kt
+++ frontend/src/main/kotlin/microscenery/Util.kt
@@ -1,9 +1,14 @@
 package microscenery
 
+import fromScenery.utils.extensions.xyz
+import graphics.scenery.Box
 import graphics.scenery.Node
 import graphics.scenery.Scene
 import graphics.scenery.controls.OpenVRHMD
 import microscenery.VRUI.behaviors.AnalogInputWrapper
+import org.joml.Vector2f
+import org.joml.Vector3f
+import org.joml.Vector4f
 import org.scijava.ui.behaviour.Behaviour
 import org.scijava.ui.behaviour.DragBehaviour
 
@@ -31,4 +36,38 @@ fun wrapForAnalogInputIfNeeded(
  */
 fun Node.detach(){
     this.parent?.removeChild(this)
+}
+
+/**
+ * @return corners of clipping rectangle in world space
+ */
+fun Box.intersectZPlane(zPlane: Float): List<Vector3f>?{
+    // origin of a box is in the middle
+    val spatial = this.spatial()
+
+    // MS = model space of box
+    val planeMS4f = Vector4f(spatial.worldPosition(), 1f)
+    planeMS4f.z = zPlane
+    planeMS4f.mul(spatial.world.copy().invert())
+
+    if (planeMS4f.z > sizes.z * 0.5 || planeMS4f.z < sizes.z * -0.5) {
+        // box and plane do not intersect
+        return null
+    }
+
+    val planeMS3f = planeMS4f.xyz()
+
+    val tl = Vector2f(-0.5f * sizes.x(), -0.5f * sizes.y())
+    val br = Vector2f( 0.5f * sizes.x() , 0.5f * sizes.y())
+
+
+    val cornersMS = listOf(
+        Vector3f(tl, 0f),
+        Vector3f(br.x,tl.y,0f),
+        Vector3f(br,0f),
+        Vector3f(tl.x, br.y, 0f)
+    )
+    //todo
+    return emptyList()
+
 }
--- frontend/src/test/kotlin/microscenery/example/T1HeadVR.kt
+++ frontend/src/test/kotlin/microscenery/example/T1HeadVR.kt
@@ -5,8 +5,8 @@ import graphics.scenery.volumes.Colormap
 import graphics.scenery.volumes.TransferFunction
 import graphics.scenery.volumes.Volume
 import microscenery.DefaultVRScene
+import microscenery.VRUI.ModifiableBox
 import microscenery.VRUI.VRUIManager
-import org.joml.Vector3f
 import kotlin.io.path.Path
 
 /**
@@ -18,7 +18,7 @@ class T1HeadVR : DefaultVRScene(T1HeadVR::class.java.simpleName) {
     override fun init() {
         prepareVRScene()
 
-        cam.spatial().position = Vector3f(0f, -5f, 5f)
+        //cam.spatial().position = Vector3f(0f, -5f, 5f)
 
 
         val volume = Volume.fromPath(Path("""C:\Users\JanCasus\volumes\t1-head.tif"""), hub)
@@ -26,6 +26,8 @@ class T1HeadVR : DefaultVRScene(T1HeadVR::class.java.simpleName) {
 //        volume.spatial().scale= Vector3f(0.1f,0.1f,0.4f)
         volume.colormap = Colormap.get("plasma")
         volume.transferFunction = TransferFunction.ramp(0.0017f, 1f, 0.01f)
+
+        scene.addChild(ModifiableBox())
 
     }
 
