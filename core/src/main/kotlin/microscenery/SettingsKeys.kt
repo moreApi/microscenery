1a77df01b345e66b48d032d6bbb949068019dce6
Jan T <moreApi@users.noreply.github.com>
2023-08-28 12:02:38 +0200

    SettingsKeys: Introduce SettigsKeys

---
+++ core/src/main/kotlin/microscenery/SettingsKeys.kt
@@ -0,0 +1,14 @@
+package microscenery
+
+object Settings {
+    object VRToolbox {
+        const val SlicingEnabled: String = "VRToolBox.SlicingEnabled"
+        const val DrawPointsEnabled: String = "DrawPointsEnabled"
+        const val DrawLineEnabled: String = "VRToolBox.DrawLineEnabled"
+        const val PathAblationEnabled = "VRToolBox.PathAblationEnabled"
+        const val PointAblationEnabled: String = "VRToolBox.PointAblationEnabled"
+        const val BubblesEnabled: String= "VRToolBox.BubblesEnabled"
+        const val OptionsEnabled: String = "VRToolBox.OptionsEnabled"
+        const val ColorChooserEnabled: String = "VRToolBox.ColorChooserEnabled"
+    }
+}
