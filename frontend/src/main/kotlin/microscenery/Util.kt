5b3006d38ca1b5081fbdc59d5ed3ab4a10509b84
Jan T <moreApi@users.noreply.github.com>
2023-09-13 17:37:11 +0200

    Util: show message with textbox

--- frontend/src/main/kotlin/microscenery/Util.kt
+++ frontend/src/main/kotlin/microscenery/Util.kt
@@ -1,14 +1,17 @@
 package microscenery
 
+import graphics.scenery.Camera
 import graphics.scenery.Node
 import graphics.scenery.Scene
 import graphics.scenery.attribute.material.HasMaterial
 import graphics.scenery.controls.OpenVRHMD
 import graphics.scenery.controls.behaviours.Touchable
+import microscenery.VRUI.Gui3D.TextBox
 import microscenery.VRUI.behaviors.AnalogInputWrapper
 import org.joml.Vector3f
 import org.scijava.ui.behaviour.Behaviour
 import org.scijava.ui.behaviour.DragBehaviour
+import kotlin.concurrent.thread
 
 
 fun wrapForAnalogInputIfNeeded(
@@ -43,5 +46,34 @@ fun HasMaterial.changeColorWithTouchable(newColor: Vector3f){
         touch.originalDiffuse = newColor
     } else {
         this.material().diffuse = newColor
+    }
+}
+
+/**
+ * Shows a [message] to the user, at a distance of [distance] meters.
+ * The message can by styled by [size] (in meters).
+ *
+ * It will be shown for [duration] milliseconds, with a default of 3000.
+ */
+@JvmOverloads fun Camera.showMessage2(message: String, distance: Float = 0.75f, size: Float = 0.05f, duration: Int = 3000) {
+    val tb = TextBox(message)
+    tb.spatial {
+        scale = Vector3f(size, size, size)
+        position = Vector3f(0.0f, 0.0f, -1.0f * distance)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    val messages = metadata.getOrPut("messages", { mutableListOf<Node>() }) as? MutableList<Node>?
+    messages?.forEach { this.removeChild(it) }
+    messages?.clear()
+
+    messages?.add(tb)
+    this.addChild(tb)
+
+    thread {
+        Thread.sleep(duration.toLong())
+
+        this.removeChild(tb)
+        messages?.remove(tb)
     }
 }
