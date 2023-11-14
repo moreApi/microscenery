d935e22c93c553fa5724d989a36d1ec6fe21c6ef
Jan T <moreApi@users.noreply.github.com>
2023-11-14 15:38:57 +0100

    EventChannel, PropertyChangeObservable: C# style events and property change listener

---
+++ frontend/src/main/kotlin/microscenery/UI/PropertyChangeObservable.kt
@@ -0,0 +1,17 @@
+package microscenery.UI
+
+import kotlin.properties.Delegates
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class PropertyChangeEvent<T>(val kProperty: KProperty<*>, val old: T, val new: T)
+
+open class PropertyChangeObservable{
+    val changeEvents = EventChannel<PropertyChangeEvent<Any>>()
+
+    internal fun <T : Any>propertyObservable(init: T): ReadWriteProperty<Any?, T> {
+        return Delegates.observable(init) { kProperty: KProperty<*>, t: T, t1: T ->
+            changeEvents.emit(PropertyChangeEvent(kProperty, t, t1))
+        }
+    }
+}
