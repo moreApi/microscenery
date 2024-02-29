aa56df04f8dba30a9a80e126765bd0e9d32e9195
Jan Tiemann <j.tiemann@hzdr.de>
2024-02-29 18:27:27 +0100

    build+gradle: update to recent scenery version and java 21 and gradle 8.6

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -18,9 +18,8 @@ dependencies {
     // This should point to the most recent commit of scenery:jans-dirtier-branch
     // At the moment scenery:jans-dirtier-branch should be a merge of:
     // - branch-of-jan
-    // - fix/objConventions
-    // (- LineColorFix)
-    implementation("graphics.scenery:scenery:060f7f3")
+    // - volume-enhancements
+    implementation("graphics.scenery:scenery:0133c94")
     // necessary for logging to work correctly
     runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
 
@@ -28,9 +27,9 @@ dependencies {
     implementation(project(":zenSysConCon"))
     implementation(files("../core/manualLib/MMCoreJ.jar"))
 
-//    implementation("org.bytedeco:ffmpeg:4.3.2-1.5.5", ffmpegNatives)
-//    implementation("org.bytedeco.javacpp-presets:ffmpeg:4.1-1.4.4")
-//    implementation("org.bytedeco.javacpp-presets:ffmpeg-platform:4.1-1.4.4")
+    implementation("org.yaml:snakeyaml") {
+        version { strictly("1.33") }
+    }
 
     testImplementation("net.imagej:ij:1.53k")
     testImplementation("net.imagej:imagej-ops:0.45.5")
@@ -42,18 +41,23 @@ dependencies {
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
     testImplementation("org.lwjgl:lwjgl-jawt:3.3.1")
     testImplementation(kotlin("test"))
-    //testImplementation(kotlin("test-junit"))
 }
 
 
 tasks.test {
     jvmArgs = listOf("-Xmx28G")
-    //useJUnit()
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+tasks{
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "21"
+    }
+
+    withType<JavaCompile>().all {
+        targetCompatibility = "21"
+        sourceCompatibility = "21"
+    }
 }
 
 tasks{
