a8c779c1f96ca335ba82e10c9de23a77ff012fcc
Jan Tiemann <j.tiemann@hzdr.de>
2024-07-12 17:51:03 +0200

    measure tool debug scene

---
+++ frontend/src/test/kotlin/microscenery/debug/MeasureToolDebug.kt
@@ -0,0 +1,108 @@
+package microscenery.debug
+
+import fromScenery.utils.extensions.times
+import graphics.scenery.DefaultNode
+import graphics.scenery.RichNode
+import graphics.scenery.controls.OpenVRHMD
+import graphics.scenery.controls.TrackedDevice
+import graphics.scenery.controls.TrackedDeviceType
+import graphics.scenery.controls.behaviours.PerButtonPressable
+import graphics.scenery.controls.behaviours.Pressable
+import graphics.scenery.controls.behaviours.SimplePressable
+import graphics.scenery.numerics.Random
+import microscenery.*
+import microscenery.UI.UIModel
+import microscenery.VRUI.Gui3D.*
+import microscenery.VRUI.MeasureTool
+import microscenery.primitives.LineNode
+import microscenery.stageSpace.MicroscopeLayout
+import microscenery.stageSpace.StageSpaceManager
+import org.joml.Matrix4f
+import org.joml.Vector3f
+import org.scijava.ui.behaviour.DragBehaviour
+import kotlin.concurrent.thread
+import kotlin.math.PI
+
+/**
+ * to use in desktop mode use:
+ * https://www.reddit.com/r/Vive/comments/6uo053/how_to_use_steamvr_tracked_devices_without_a_hmd/
+ */
+class MeasureToolDebug() : DefaultScene(VR = true, width = 1024, height = 1024) {
+    val msHub = MicrosceneryHub(hub)
+    val uiModel = msHub.getAttribute(UIModel::class.java)
+    init {
+        MicroscenerySettings.set(Settings.StageSpace.ShowHullbox,true)
+    }
+    override fun init() {
+        super.init()
+
+        cam.spatial().position = Vector3f(0f,0f,2f)
+        val root = RichNode()
+        scene.addChild(root)
+
+
+
+        val hw = DemoMicroscopeHardware(binning = 1)
+        val stageSpaceManager = StageSpaceManager(
+            hw,
+            scene,
+            msHub,
+            layout = MicroscopeLayout.Default(MicroscopeLayout.Axis.Z)
+        )
+
+        val measureTool = MeasureTool(stageSpaceManager = stageSpaceManager,hmd = hmd)
+        scene.addChild(measureTool)
+
+        thread {
+            Thread.sleep(3000)
+
+            (0..4).map { measureTool.apply {
+                this.spatial().position = Random.random3DVectorFromRange(-3.0f,3.0f)
+                this.spatial().scale = Vector3f(0.3f)
+                (this.getAttributeOrNull(Pressable::class.java) as? PerButtonPressable)?.actions?.get(OpenVRHMD.OpenVRButton.Trigger)?.onRelease?.invoke(this.spatial(), TrackedDevice(TrackedDeviceType.Controller,"asd",Matrix4f(),System.nanoTime()))
+            } }
+
+            val lNodes = scene.findByClassname("MeasurePoint").mapNotNull{ it as? MeasureTool.MeasurePoint }
+
+            while (true){
+                lNodes[2].spatial().position = Vector3f(3f) * ((System.currentTimeMillis() % 5000) / 5000f)
+                root.rotation.rotationY(PI.toFloat() * 2f * ((System.currentTimeMillis() % 5000) / 5000f))
+                root.spatial().needsUpdate = true
+            }
+        }
+    }
+
+    override fun inputSetup() {
+        super.inputSetup()
+        // debug: select 3dGUI menu entries with mouse
+        inputHandler?.addBehaviour("debug3dClick", object : DragBehaviour {
+            var pressable: SimplePressable? = null
+            val controllerDevice = TrackedDevice(TrackedDeviceType.Generic,"dummy", Matrix4f().identity(),System.nanoTime())
+
+            override fun init(p0: Int, p1: Int) {
+                pressable = cam.getNodesForScreenSpacePosition(p0,p1).matches
+                    .firstNotNullOfOrNull { it.node.getAttributeOrNull(Pressable::class.java) as? SimplePressable }
+                pressable?.onPress?.invoke(cam.spatial(),controllerDevice)
+
+            }
+            override fun drag(p0: Int, p1: Int) {
+                pressable?.onHold?.invoke(cam.spatial(),controllerDevice)
+            }
+
+            override fun end(p0: Int, p1: Int) {
+                pressable?.onRelease?.invoke(cam.spatial(), controllerDevice)
+                pressable = null
+            }
+        })
+        inputHandler?.addKeyBinding("debug3dClick","1")
+
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            MeasureToolDebug().main()
+        }
+    }
+}
