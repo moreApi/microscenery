e655d4aee01f45fd0c0d59ffb9558527c848e8e7
Jan Tiemann <j.tiemann@hzdr.de>
2024-07-16 18:13:47 +0200

    MeasureTool, WorldToLocalPosSync: correct labels

--- frontend/src/main/kotlin/microscenery/VRUI/MeasureTool.kt
+++ frontend/src/main/kotlin/microscenery/VRUI/MeasureTool.kt
@@ -6,29 +6,20 @@ import graphics.scenery.attribute.material.DefaultMaterial
 import graphics.scenery.attribute.spatial.Spatial
 import graphics.scenery.controls.OpenVRHMD
 import graphics.scenery.controls.behaviours.*
+import microscenery.VRUI.Gui3D.Row
 import microscenery.VRUI.Gui3D.TextBox
 import microscenery.VRUI.fromScenery.VRFastSelectionWheel
 import microscenery.detach
 import microscenery.primitives.LineNode
 import microscenery.stageSpace.StageSpaceManager
-import org.joml.Quaternionf
+import microscenery.stageSpace.WorldToLocalPosSync
 import org.joml.Vector3f
-import kotlin.math.PI
 
 
 /**
- * V1:
- * If no measure point has been placed prepare one on the tip.
- * If only one is placed prepare the next one on the tip and connect it to the first one.
- * If a connection is present show its length in microscope coordinates.
- * If two points are placed don't automatically prepare a new point at the tip.
- *      If a point gets dragged (either by hand or tool tip) move the dot.
- *      If empty space is clicked prepare a new point and connect it to the last point.
- * Pressing secondary button delets the point at tip
- *  and if none is present the one the tool is pointed at
- *  and if none is present the menu is opened
+ * VR measuring tool
  *
- *  V2:
+ * Behavior:
  *  never have a point prepared
  *  on trigger:
  *      place point when not touching one
@@ -38,7 +29,7 @@ import kotlin.math.PI
 class MeasureTool(
     var lineColor: Vector3f = Vector3f(1.0f, 0.5f, 0.0f),
     val stageSpaceManager: StageSpaceManager,
-    hmd: OpenVRHMD
+    val hmd: OpenVRHMD
 ) : Box(Vector3f(0.05f, 0.13f, 0.05f)), VRHandTool {
     private val tip: Box
     private val pointOutput: RichNode
@@ -48,25 +39,22 @@ class MeasureTool(
     private var menu: VRFastSelectionWheel? = null
 
     init {
-
-        material().diffuse = Vector3f(1f)
-
         val tipLength = 0.025f
         tip = Box(Vector3f(0.015f, tipLength, 0.015f))
         tip.spatial().position = Vector3f(0f, this.sizes.y / 2 + tipLength / 2, 0f)
-        tip.material().diffuse = Vector3f(1f)
+        tip.material().diffuse = lineColor
         addChild(tip)
         pointOutput = RichNode()
         pointOutput.spatial().position.y = tipLength / 2
         tip.addChild(pointOutput)
-        val touch = Touch("Measuring Tool touch", tip, {measurePoints})
+        val touch = Touch("Measuring Tool touch", tip, { measurePoints })
 
         //this.initVRHandToolAndPressable(uiModel,
         this.addAttribute(Pressable::class.java, PerButtonPressable(
             mapOf(
                 OpenVRHMD.OpenVRButton.Trigger to SimplePressable(
                     onRelease = { _, _ ->
-                        if (touch.selected.isNotEmpty()){
+                        if (touch.selected.isNotEmpty()) {
                             deletePoint(touch.selected.first() as? MeasurePoint)
                         } else {
                             placePoint()
@@ -107,8 +95,9 @@ class MeasureTool(
 
     private fun placePoint() {
         val newPoint = MeasurePoint()
+        newPoint.material().diffuse = lineColor
         val worldPos = pointOutput.spatial().worldPosition()
-        newPoint.spatial().position = stageSpaceManager.worldToStageSpace(worldPos,true)
+        newPoint.spatial().position = stageSpaceManager.worldToStageSpace(worldPos, true)
         stageSpaceManager.stageRoot.addChild(newPoint)
 
         measurePoints.lastOrNull()?.connectTo(newPoint)
@@ -117,27 +106,36 @@ class MeasureTool(
         newPoint.lines.filter {
             it.getAttributeOrNull(MeasureLabel::class.java) == null
         }.forEach { newLine ->
-            val length = scale.y * (parent?.spatialOrNull()?.scale?.y ?: 1f)
-            val label = TextBox("$length mu", thickness = 0.2f)
-            label.spatial {
-                position.y = 1f
-                rotation = Quaternionf().rotationZ(PI.toFloat())
-            }
-            label.update += {
-                val l = scale.y * (parent?.spatialOrNull()?.scale?.y ?: 1f)
-                label.text = "$l mu"
-            }
-            newLine.addChild(label)
-            newLine.addAttribute(MeasureLabel::class.java, MeasureLabel())
+            val lineMidpoint = RichNode()
+            lineMidpoint.spatial().position = Vector3f(0f, 0.5f, 0f)
+            newLine.addChild(lineMidpoint)
 
+            val labelPivot = RichNode()
+            WorldToLocalPosSync(lineMidpoint, labelPivot)
+            labelPivot.spatial {
+                position.y = 0.01f
+                scale = Vector3f(0.05f)
+            }
+            newLine.getScene()?.addChild(labelPivot)
+
+            val label = TextBox("x mu", thickness = 0.2f)
+            newLine.update += {
+                val l = newLine.spatial().scale.y
+                label.text = "%.3f".format(l) + " mu"
+            }
+            labelPivot.addChild(Row(label))
+
+            newLine.addAttribute(MeasureLabel::class.java, MeasureLabel())
         }
     }
 
     private fun deletePoint(point: MeasurePoint?) {
         val index = measurePoints.indexOf(point)
-        if (index < 0) {return}
-        val prev = measurePoints.getOrNull(index -1)
-        val next = measurePoints.getOrNull(index +1)
+        if (index < 0) {
+            return
+        }
+        val prev = measurePoints.getOrNull(index - 1)
+        val next = measurePoints.getOrNull(index + 1)
         measurePoints -= point!!
 
         prev?.removeConnection(point)
@@ -154,14 +152,14 @@ class MeasureTool(
         point.detach()
     }
 
-    private fun clearAll(){
+    private fun clearAll() {
         measurePoints.forEach {
             it.detach()
         }
         measurePoints = emptyList()
     }
 
-    class MeasurePoint : LineNode(){
+    class MeasurePoint : LineNode() {
         init {
             setMaterial(DefaultMaterial().apply { diffuse = Vector3f(0.7f) })
         }
--- frontend/src/test/kotlin/microscenery/debug/MeasureToolDebug.kt
+++ frontend/src/test/kotlin/microscenery/debug/MeasureToolDebug.kt
@@ -1,7 +1,6 @@
 package microscenery.debug
 
 import fromScenery.utils.extensions.times
-import graphics.scenery.DefaultNode
 import graphics.scenery.RichNode
 import graphics.scenery.controls.OpenVRHMD
 import graphics.scenery.controls.TrackedDevice
@@ -12,34 +11,31 @@ import graphics.scenery.controls.behaviours.SimplePressable
 import graphics.scenery.numerics.Random
 import microscenery.*
 import microscenery.UI.UIModel
-import microscenery.VRUI.Gui3D.*
 import microscenery.VRUI.MeasureTool
-import microscenery.primitives.LineNode
 import microscenery.stageSpace.MicroscopeLayout
 import microscenery.stageSpace.StageSpaceManager
 import org.joml.Matrix4f
 import org.joml.Vector3f
 import org.scijava.ui.behaviour.DragBehaviour
 import kotlin.concurrent.thread
-import kotlin.math.PI
 
 /**
  * to use in desktop mode use:
  * https://www.reddit.com/r/Vive/comments/6uo053/how_to_use_steamvr_tracked_devices_without_a_hmd/
  */
-class MeasureToolDebug() : DefaultScene(VR = true, width = 1024, height = 1024) {
+class MeasureToolDebug : DefaultScene(VR = true, width = 1024, height = 1024) {
     val msHub = MicrosceneryHub(hub)
-    val uiModel = msHub.getAttribute(UIModel::class.java)
+
     init {
-        MicroscenerySettings.set(Settings.StageSpace.ShowHullbox,true)
+        MicroscenerySettings.set(Settings.StageSpace.ShowHullbox, false)
     }
+
     override fun init() {
         super.init()
 
-        cam.spatial().position = Vector3f(0f,0f,2f)
+        cam.spatial().position = Vector3f(0f, 0f, 1f)
         val root = RichNode()
         scene.addChild(root)
-
 
 
         val hw = DemoMicroscopeHardware(binning = 1)
@@ -50,24 +46,46 @@ class MeasureToolDebug() : DefaultScene(VR = true, width = 1024, height = 1024) 
             layout = MicroscopeLayout.Default(MicroscopeLayout.Axis.Z)
         )
 
-        val measureTool = MeasureTool(stageSpaceManager = stageSpaceManager,hmd = hmd)
+        val measureTool = MeasureTool(stageSpaceManager = stageSpaceManager, hmd = hmd)
         scene.addChild(measureTool)
+
+
 
         thread {
             Thread.sleep(3000)
 
-            (0..4).map { measureTool.apply {
-                this.spatial().position = Random.random3DVectorFromRange(-3.0f,3.0f)
-                this.spatial().scale = Vector3f(0.3f)
-                (this.getAttributeOrNull(Pressable::class.java) as? PerButtonPressable)?.actions?.get(OpenVRHMD.OpenVRButton.Trigger)?.onRelease?.invoke(this.spatial(), TrackedDevice(TrackedDeviceType.Controller,"asd",Matrix4f(),System.nanoTime()))
-            } }
 
-            val lNodes = scene.findByClassname("MeasurePoint").mapNotNull{ it as? MeasureTool.MeasurePoint }
+            val range = 0.15f
+            val square = listOf(
+                Vector3f(-range, range, range),
+                Vector3f(range, range, range),
+                Vector3f(range, -range, range),
+                Vector3f(-range, -range, range),
+            )
 
-            while (true){
-                lNodes[2].spatial().position = Vector3f(3f) * ((System.currentTimeMillis() % 5000) / 5000f)
-                root.rotation.rotationY(PI.toFloat() * 2f * ((System.currentTimeMillis() % 5000) / 5000f))
-                root.spatial().needsUpdate = true
+            val rnd = (0..4).map { Random.random3DVectorFromRange(-0.5f, 0.5f) }
+
+            rnd.forEach {
+                measureTool.apply {
+                    this.spatial().position = it
+                    Thread.sleep(50)
+                    //this.spatial().scale = Vector3f(0.3f)
+                    (this.getAttributeOrNull(Pressable::class.java) as? PerButtonPressable)
+                        ?.actions
+                        ?.get(OpenVRHMD.OpenVRButton.Trigger)
+                        ?.onRelease
+                        ?.invoke(
+                            this.spatial(),
+                            TrackedDevice(TrackedDeviceType.Controller, "dummy", Matrix4f(), System.nanoTime())
+                        )
+                    Thread.sleep(50)
+                }
+            }
+
+            val lNodes = scene.findByClassname("MeasurePoint").mapNotNull { it as? MeasureTool.MeasurePoint }
+
+            while (true) {
+                lNodes[2].spatial().position = Vector3f(100f) * ((System.currentTimeMillis() % 5000) / 5000f)
             }
         }
     }
@@ -77,16 +95,18 @@ class MeasureToolDebug() : DefaultScene(VR = true, width = 1024, height = 1024) 
         // debug: select 3dGUI menu entries with mouse
         inputHandler?.addBehaviour("debug3dClick", object : DragBehaviour {
             var pressable: SimplePressable? = null
-            val controllerDevice = TrackedDevice(TrackedDeviceType.Generic,"dummy", Matrix4f().identity(),System.nanoTime())
+            val controllerDevice =
+                TrackedDevice(TrackedDeviceType.Generic, "dummy", Matrix4f().identity(), System.nanoTime())
 
             override fun init(p0: Int, p1: Int) {
-                pressable = cam.getNodesForScreenSpacePosition(p0,p1).matches
+                pressable = cam.getNodesForScreenSpacePosition(p0, p1).matches
                     .firstNotNullOfOrNull { it.node.getAttributeOrNull(Pressable::class.java) as? SimplePressable }
-                pressable?.onPress?.invoke(cam.spatial(),controllerDevice)
+                pressable?.onPress?.invoke(cam.spatial(), controllerDevice)
 
             }
+
             override fun drag(p0: Int, p1: Int) {
-                pressable?.onHold?.invoke(cam.spatial(),controllerDevice)
+                pressable?.onHold?.invoke(cam.spatial(), controllerDevice)
             }
 
             override fun end(p0: Int, p1: Int) {
@@ -94,7 +114,7 @@ class MeasureToolDebug() : DefaultScene(VR = true, width = 1024, height = 1024) 
                 pressable = null
             }
         })
-        inputHandler?.addKeyBinding("debug3dClick","1")
+        inputHandler?.addKeyBinding("debug3dClick", "1")
 
 
     }
