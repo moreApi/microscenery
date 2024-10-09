da1665b7b951eb127119d25b6e84aa6cc404420d
Jan Tiemann <j.tiemann@hzdr.de>
2024-10-09 15:47:23 +0200

    Simulatable, CylinderSimulatable: add and use inverseStageMatrix to handle deeper stage graphs

--- frontend/src/main/kotlin/microscenery/simulation/CylinderSimulatable.kt
+++ frontend/src/main/kotlin/microscenery/simulation/CylinderSimulatable.kt
@@ -1,30 +1,34 @@
 package microscenery.simulation
 
 import graphics.scenery.primitives.Cylinder
-import org.joml.Matrix4f
 import org.joml.Vector2f
 import org.joml.Vector3f
 import org.joml.Vector4f
 import kotlin.math.absoluteValue
 
-class CylinderSimulatable(val parent: Cylinder, var maxIntensity: Short = 4000.toShort(), var range: Float = 15f, val hollow: Boolean = true) : Simulatable {
+class CylinderSimulatable(
+    val parent: Cylinder,
+    var maxIntensity: Short = 4000.toShort(),
+    var range: Float = 15f,
+    val hollow: Boolean = true
+) : Simulatable {
 
     override fun intensity(pos: Vector3f): Short {
-        val modelPos = Vector4f(pos,1f).mul(Matrix4f(parent.spatial().model).invertAffine())
+        val modelPos = Vector4f(pos, 1f).mul(parent.inverseStageMatrix())
 
-        var distanceFromDisc = Vector2f(modelPos.x,modelPos.z).length() - parent.radius
+        var distanceFromDisc = Vector2f(modelPos.x, modelPos.z).length() - parent.radius
         if (distanceFromDisc.absoluteValue > range) return 0 //out of range
-        distanceFromDisc = if (!hollow){
+        distanceFromDisc = if (!hollow) {
             distanceFromDisc.coerceAtLeast(0f)
         } else {
             distanceFromDisc.absoluteValue
         }
-        val valueXY = (1 - distanceFromDisc/range) * maxIntensity
+        val valueXY = (1 - distanceFromDisc / range) * maxIntensity
 
-        var modY = when{
+        val modY = when {
             modelPos.y < -range -> 0f
             modelPos.y < 0 -> 1 - modelPos.y.absoluteValue / range
-            modelPos.y <  parent.height -> 1f
+            modelPos.y < parent.height -> 1f
             modelPos.y - parent.height < range -> 1 - (modelPos.y.absoluteValue - parent.height) / range
             else -> 0f // case: modelPos.y - parent.height > range
         }
@@ -32,7 +36,7 @@ class CylinderSimulatable(val parent: Cylinder, var maxIntensity: Short = 4000.t
         return (valueXY * modY).toInt().toShort()
     }
 
-    companion object{
+    companion object {
         fun addTo(box: Cylinder): CylinderSimulatable {
             val tmp = CylinderSimulatable(box)
             box.addAttribute(Simulatable::class.java, tmp)
--- frontend/src/main/kotlin/microscenery/simulation/Simulatable.kt
+++ frontend/src/main/kotlin/microscenery/simulation/Simulatable.kt
@@ -1,5 +1,7 @@
 package microscenery.simulation
 
+import graphics.scenery.attribute.spatial.HasSpatial
+import org.joml.Matrix4f
 import org.joml.Vector3f
 
 /**
@@ -7,5 +9,18 @@ import org.joml.Vector3f
  */
 interface Simulatable {
     fun intensity(pos: Vector3f): Short
+
+
+    fun HasSpatial.inverseStageMatrix(): Matrix4f {
+        val target = Matrix4f(this.spatial().model)
+
+        var currentNode = this
+        while (currentNode.parent?.name != "stage root"){
+            currentNode = currentNode.parent as? HasSpatial ?: break
+            currentNode.spatial().model.mulAffine(target,target)
+        }
+
+        return target.invertAffine()
+    }
 }
 
