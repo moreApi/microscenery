e77fd92dbacfc9510f8d0653815f9ecb0465db5f
Jan Tiemann <j.tiemann@hzdr.de>
2024-11-08 18:10:01 +0100

    SpatialLogger: Fix spatial logging for VR Tracked devices

--- frontend/src/test/kotlin/microscenery/scenes/stageStudy/StudySpatialLogger.kt
+++ frontend/src/test/kotlin/microscenery/scenes/stageStudy/StudySpatialLogger.kt
@@ -1,6 +1,7 @@
 package microscenery.scenes.stageStudy
 
 import graphics.scenery.Camera
+import graphics.scenery.RichNode
 import graphics.scenery.attribute.spatial.Spatial
 import microscenery.Agent
 import microscenery.MicrosceneryHub
@@ -20,6 +21,11 @@ class StudySpatialLogger(val camera: Camera, val msHub: MicrosceneryHub, file: F
     val uiModel = msHub.getAttribute(UIModel::class.java)
     val stageSpaceManager = msHub.getAttribute(StageSpaceManager::class.java)
 
+    // vr spatial are weird! But their children are alright
+    val cameraSpatialBobbel = RichNode()
+    val leftControllerSpatialBobbel = RichNode()
+    val rightControllerSpatialBobbel = RichNode()
+
     val writer: BufferedWriter
 
     private val otherEvents: BlockingQueue<LoggingEvent> = ArrayBlockingQueue(50)
@@ -34,12 +40,57 @@ class StudySpatialLogger(val camera: Camera, val msHub: MicrosceneryHub, file: F
         writer.write("time;name;PosX;PosY;PosZ;RotX;RotY;RotZ;RotW")
         writer.newLine()
 
+        camera.addChild(cameraSpatialBobbel)
+
         startAgent()
     }
 
     fun logEvent(name: String,extraParams: List<String>? = null, time: Long = System.currentTimeMillis()){
         otherEvents.put(LoggingEvent(name,extraParams,time))
     }
+
+    override fun onLoop() {
+        writeOutOtherEvents()
+        writeSpatialLog("camera", cameraSpatialBobbel.spatial())
+        writer.newLine()
+        writeSpatialLog("focusTarget", stageSpaceManager.focusManager.focusTarget.spatial())
+        writer.newLine()
+        uiModel.leftVRController?.model?.let {
+            if (leftControllerSpatialBobbel.parent == null) it.addChild(leftControllerSpatialBobbel)
+            writeSpatialLog("leftHand",leftControllerSpatialBobbel.spatial())
+            writer.newLine()
+        }
+        uiModel.rightVRController?.model?.let {
+            if (rightControllerSpatialBobbel.parent == null) it.addChild(rightControllerSpatialBobbel)
+            writeSpatialLog("rightHand",rightControllerSpatialBobbel.spatial())
+            writer.newLine()
+        }
+        writer.newLine()
+        writer.flush()
+        Thread.sleep(loggingInterval)
+    }
+
+
+    private fun writeOutOtherEvents() {
+        while (otherEvents.isNotEmpty()) {
+            val e = otherEvents.poll()
+            writer.write(e.time.toString() + ";" + e.name + e.extraParams?.let { ";" + it.joinToString(";") })
+            writer.newLine()
+        }
+        writer.flush()
+    }
+
+    private fun writeSpatialLog(name: String, spatial: Spatial) {
+        val logLine = System.currentTimeMillis().toString() + ";$name;" + spatial.toCSVString()
+        writer.write(logLine)
+    }
+
+    override fun onClose() {
+        writeOutOtherEvents()
+        writer.close()
+    }
+
+    data class LoggingEvent(val name: String,val extraParams: List<String>? = null, val time: Long = System.currentTimeMillis())
 
     companion object{
         fun Spatial.toCSVString():String{
@@ -52,39 +103,4 @@ class StudySpatialLogger(val camera: Camera, val msHub: MicrosceneryHub, file: F
             return "%.4f".format(x) + ";" + "%.4f".format(y) + ";" + "%.4f".format(z) + ";" + "%.4f".format(w)
         }
     }
-
-    override fun onLoop() {
-        writeOutOtherEvents()
-        writeLog("camera", camera.spatial())
-        writer.newLine()
-        writeLog("focusTarget", stageSpaceManager.focusManager.focusTarget.spatial())
-        writer.newLine()
-        uiModel.leftVRController?.model?.spatialOrNull()?.let { writeLog("leftHand",it)
-            writer.newLine() }
-        uiModel.rightVRController?.model?.spatialOrNull()?.let { writeLog("rightHand",it)
-            writer.newLine() }
-        writer.newLine()
-        writer.flush()
-        Thread.sleep(loggingInterval)
-    }
-
-    private fun writeOutOtherEvents() {
-        while (otherEvents.isNotEmpty()) {
-            val e = otherEvents.poll()
-            writer.write(e.time.toString() + ";" + e.name + e.extraParams?.let { ";" + it.joinToString(";") })
-            writer.newLine()
-        }
-    }
-
-    private fun writeLog(name: String, spatial: Spatial) {
-        val logLine = System.currentTimeMillis().toString() + ";$name;" + spatial.toCSVString()
-        writer.write(logLine)
-    }
-
-    override fun onClose() {
-        writeOutOtherEvents()
-        writer.close()
-    }
-
-    data class LoggingEvent(val name: String,val extraParams: List<String>? = null, val time: Long = System.currentTimeMillis())
 }
