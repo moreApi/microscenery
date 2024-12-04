89e8e7ea4b3de6f03df6bc41a1c6d71dd7ef93e4
Jan Tiemann <j.tiemann@hzdr.de>
2024-12-04 19:17:23 +0100

    TargetPositionExtractor: created

--- frontend/src/test/kotlin/microscenery/scenes/stageStudy/Orchestration/TrialGenerator.kt
+++ frontend/src/test/kotlin/microscenery/scenes/stageStudy/Orchestration/TrialGenerator.kt
@@ -6,31 +6,35 @@ import kotlin.random.Random
 
 object TrialGenerator {
 
-    fun generateConfig(): TrialConfig {
-        val axonParams: List<Scenario.Axon> = listOf(
-            Scenario.Axon( // 6T
-                3229621274613467545, listOf(0f, -0.75f, 0f).toFloatArray(), 250f, 3, 1, 2
-            ), Scenario.Axon( // 7 targets
-                3824716, listOf(0f, -0.5f, 0f).toFloatArray(), 350f, 3, 1, 3
-            ), Scenario.Axon( // 6T
-                -8036747134749984232, listOf(0f, -0.75f, 0f).toFloatArray(), 250f, 4, 1, 2
-            ), Scenario.Axon( //7T
-                3449888335543516408, listOf(0f, -0.65f, 0f).toFloatArray(), 400f, 3, 1, 2
-            ), Scenario.Axon( //T6
-                -3726482736712866052, listOf(0f, -0.65f, 0f).toFloatArray(), 350f, 3, 1, 2
-            ), Scenario.Axon( //T6
-                -3149103814322066555, listOf(0f, -0.65f, 0f).toFloatArray(), 350f, 3, 1, 2
-            )
-        ).shuffled()
+    val baseAxonParams = listOf(
+        Scenario.Axon( // 6T
+            3229621274613467545, listOf(0f, -0.75f, 0f).toFloatArray(), 250f, 3, 1, 2
+        ), Scenario.Axon( // 7 targets
+            3824716, listOf(0f, -0.5f, 0f).toFloatArray(), 350f, 3, 1, 3
+        ), Scenario.Axon( // 6T
+            -8036747134749984232, listOf(0f, -0.75f, 0f).toFloatArray(), 250f, 4, 1, 2
+        ), Scenario.Axon( //7T
+            3449888335543516408, listOf(0f, -0.65f, 0f).toFloatArray(), 400f, 3, 1, 2
+        ), Scenario.Axon( //T6
+            -3726482736712866052, listOf(0f, -0.65f, 0f).toFloatArray(), 350f, 3, 1, 2
+        ), Scenario.Axon( //T6
+            -3149103814322066555, listOf(0f, -0.65f, 0f).toFloatArray(), 350f, 3, 1, 2
+        )
+    )
 
-        val tubeParams: List<Scenario.Tube> = listOf(
-            Scenario.Tube(123),
-            Scenario.Tube(37),
-            Scenario.Tube(42),
-            Scenario.Tube(69),
-            Scenario.Tube(-1),
-            Scenario.Tube(314159265358979)
-        ).shuffled()
+    val baseTubeParams = listOf(
+        Scenario.Tube(123),
+        Scenario.Tube(37),
+        Scenario.Tube(42),
+        Scenario.Tube(69),
+        Scenario.Tube(-1),
+        Scenario.Tube(314159265358979)
+    )
+
+    fun generateConfig(): TrialConfig {
+        val axonParams: List<Scenario.Axon> = baseAxonParams.shuffled()
+
+        val tubeParams: List<Scenario.Tube> = baseTubeParams.shuffled()
 
         val sdf = SimpleDateFormat("yyyy-MM-dd--hh-mm-ss")
         val currentDate = sdf.format(Date())
