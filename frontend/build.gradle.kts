9488f23ca66473009d58dec644c7dd0ff4f9634d
Jan Tiemann <j.tiemann@hzdr.de>
2025-07-11 12:36:21 +0200

    build.gradle: fix logger issue

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -9,8 +9,8 @@ group = "me.jancasus"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://jitpack.io")
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://maven.scijava.org/content/groups/public")
 }
 
@@ -25,7 +25,7 @@ dependencies {
     // - fix/attachment-order-in-dssdo-shaders
     implementation("com.github.scenerygraphics:scenery:d2c7e737ad63b2ba7cb05c42c9c0e09acb6e63ca")
     // necessary for logging to work correctly
-    runtimeOnly("org.slf4j:slf4j-simple:1.7.30")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.25.0")
 
     implementation(project(":core"))
     implementation(project(":zenSysConCon"))
