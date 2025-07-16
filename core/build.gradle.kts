62f59960ec34337fb4ee3ede25ee2e8504a92742
Jan T <j.tiemann@hzdr.de>
2025-07-16 15:32:56 +0200

    core build: mac protobuf exception

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -20,7 +20,11 @@ dependencies {
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
     implementation("org.joml:joml:1.10.5")
-    implementation ("com.google.protobuf:protobuf-java:3.25.3")
+    if(System.getProperty("os.name").lowercase().contains("mac")) {
+        implementation ("com.google.protobuf:protobuf-java:4.29.1") //current macOS homebrew version
+    } else {
+        implementation ("com.google.protobuf:protobuf-java:3.25.3")
+    }
     implementation("com.google.protobuf:protobuf-java-util:3.25.3")
     implementation("org.zeromq:jeromq:0.5.2")
     implementation ("com.github.stuhlmeier:kotlin-events:v2.0")
